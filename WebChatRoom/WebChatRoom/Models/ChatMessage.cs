b4b4f6710ced3239d1dd0dfdf9629e4e1e45dc01
Rasmus <rasmusjensen.dev@gmail.com>
2024-11-16 19:43:46 +0100

    Initial chat look

---
+++ WebChatRoom/WebChatRoom/Models/ChatMessage.cs
@@ -0,0 +1,10 @@
+namespace WebChatRoom.Models
+{
+    public class ChatMessage
+    {
+        public string Sender { get; set; }
+        public string Timestamp { get; set; }
+        public string MessageBody { get; set; }
+    }
+
+}
