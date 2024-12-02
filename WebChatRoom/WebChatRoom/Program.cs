2c0305ab264f56f0b14f5eaea0e06fb028cb4127
Rasmus <rasmusjensen.dev@gmail.com>
2024-12-02 08:01:21 +0100

    Small refactor nothing much

--- WebChatRoom/WebChatRoom/Program.cs
+++ WebChatRoom/WebChatRoom/Program.cs
@@ -2,34 +2,34 @@ using WebChatRoom.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR(); // Add SignalR services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-} else
+}
+else
 {
-    app.UseDeveloperExceptionPage(); // Enable detailed errors
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Map routes
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapHub<ChatHub>("/chatHub"); // Map the ChatHub endpoint
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
