eff7156ec8e3dfb38b3a4e2f1b1eb86230dd49e0
Rasmus <rasmusjensen.dev@gmail.com>
2024-12-02 08:02:52 +0100

    Added a singleton class to hold all the data in memory

---
+++ WebChatRoom/WebChatRoom/Singletons/ApplicationSingleton.cs
@@ -0,0 +1,26 @@
+﻿using WebChatRoom.Models;
+
+namespace WebChatRoom.Singletons
+{
+    public static class ApplicationSingleton
+    {
+        public static HashSet<string> Usernames { get; private set; } = new();
+        public static List<ChatMessage> Messages { get; private set; } = new();
+
+        public static void ClearCache()
+        {
+            Usernames.Clear();
+            Messages.Clear();
+        }
+
+        public static bool AddUsername(string newUsername)
+        {
+            return Usernames.Add(newUsername);
+        }
+
+        public static void AddChatMessage(ChatMessage message)
+        {
+            Messages.Add(message);
+        }
+    }
+}
