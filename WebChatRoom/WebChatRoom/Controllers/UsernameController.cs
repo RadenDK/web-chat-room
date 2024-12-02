4a7d109ed435bbaa7ded5554cc8a621958dc752f
Rasmus <rasmusjensen.dev@gmail.com>
2024-12-02 08:01:38 +0100

    Added a usernamecontroller to check if usernames are valid or not

---
+++ WebChatRoom/WebChatRoom/Controllers/UsernameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebChatRoom.Singletons;
+
+namespace WebChatRoom.Controllers
+{
+    [Route("[controller]")]
+    public class UsernameController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult AddUsername(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                return BadRequest("Username cannot be empty.");
+            }
+
+            // Check if the username exists
+            if (!ApplicationSingleton.AddUsername(username))
+            {
+                return BadRequest("Username already exists today.");
+            }
+
+            return Ok("Username added successfully.");
+        }
+    }
+}
