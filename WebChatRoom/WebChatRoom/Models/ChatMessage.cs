4bfeba08a18022e88098d7170018b8acc33ecf2b
Rasmus <rasmusjensen.dev@gmail.com>
2024-12-02 08:02:35 +0100

    Added attributes to properties so that the javascript can map the values right

--- WebChatRoom/WebChatRoom/Models/ChatMessage.cs
+++ WebChatRoom/WebChatRoom/Models/ChatMessage.cs
@@ -1,9 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace WebChatRoom.Models
 {
     public class ChatMessage
     {
+        [JsonPropertyName("sender")]
         public string Sender { get; set; }
-        public string Timestamp { get; set; }
+
+        [JsonPropertyName("timestamp")]
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("message")]
         public string Message { get; set; }
     }
 }
