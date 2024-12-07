240afc60f9a3f5edb35fbbdd78f542f2cea6a942
Rasmus <rasmusjensen.dev@gmail.com>
2024-12-07 11:37:05 +0100

    Made the script able to save the image to my machine

--- CuteSmsBot/ImageRetriver.php
+++ CuteSmsBot/ImageRetriver.php
@@ -3,19 +3,73 @@
 // This includes Composer's autoloader, which automatically loads all installed libraries.
 require __DIR__ . '/vendor/autoload.php';
 
-// This uses the phpdotenv library to load variables from a .env file in the current directory
-// into the PHP environment for this script's runtime.
-// It returns an instance of the Dotenv class.
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+use GuzzleHttp\Client;
+use Dotenv\Dotenv;
 
-// Calls the safeLoad method on the Dotenv object to load the .env file.
-// This could also have been written as a one-liner: 
-// Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
+$dotenv = Dotenv::createImmutable(__DIR__);
+
 $dotenv->safeLoad();
 
-// At this point, the superglobal $_ENV contains all variables defined in the .env file.
-// For example, if your .env file has "BING_IMAGE_SEARCH_API_KEY=your-api-key",
-// you can now access it using $_ENV['BING_IMAGE_SEARCH_API_KEY'].
-echo $_ENV['BING_IMAGE_SEARCH_API_KEY'];
+function println($text) {
+    echo $text . PHP_EOL;
+}
+
+function getImageUrl($query_term)
+{
+    $api_key = $_ENV['BING_IMAGE_SEARCH_API_KEY'];
+
+    $base_url = 'https://api.bing.microsoft.com/v7.0/images/search';
+
+    $client = new Client();
+
+    println("Sending a request to the bing image search api");
+    $response = $client->request('GET', $base_url, [
+        'headers' => ['Ocp-Apim-Subscription-Key' => $api_key],
+
+        'query' => [
+            'q' => $query_term,
+            'count' => 1, // Number of images to retrieve
+            'offset' => rand(0, 100), // Start position
+            'safeSearch' => 'Strict',
+            'imageType' => 'Photo', // Filter by image type
+            'maxFileSize' => 520192
+        ]
+    ]);
+
+    $response_json = json_decode($response->getBody());
+
+    $img_url = $response_json->value[0]->contentUrl;
+
+    println("Got the following URL " . $img_url);
+
+    return $img_url;
+}
+
+function getJPGImageUrl($query_term)
+{
+    $image_url = "";
+    do {
+        $image_url = getImageUrl($query_term);
+    }
+    while (!str_ends_with($image_url, ".jpg"));
+
+    return $image_url;
+}
+
+function saveImgUrlToFileInCurrentDirectory($img_url, $file_name)
+{
+    $file_path = __DIR__ . "\\" . $file_name;
+
+    $img_data = file_get_contents($img_url);
+    file_put_contents($file_path, $img_data);
+
+    println("Saved the url " . $img_url . " at " . $file_path . "");
+}
+
+$query_term = "cute bunny";
+$img_url = getJPGImageUrl($query_term);
+
+$file_name = "cute_img.jpg";
+saveImgUrlToFileInCurrentDirectory($img_url, $file_name);
 
 ?>
